a2d4e02cb25d3bad204501349084f92920b41b80
Babajide Muritala <jide142000@yahoo.com>
2024-06-13 20:05:09 +0200

    show only 3jobs on homepage

--- src/App.jsx
+++ src/App.jsx
@@ -11,16 +11,6 @@ const App = () => {
       <HomeCards />
       <JobListings />
 
-
-
-
-    <section className="m-auto max-w-lg my-10 px-6">
-      <a
-        href="jobs.html"
-        className="block bg-black text-white text-center py-4 px-6 rounded-xl hover:bg-gray-700"
-        >View All Jobs</a
-      >
-    </section>
     </>
   )
 }
